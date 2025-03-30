16b80ba8d2c1f8b83035d16586d87d738a1c0960
warek <alexandrudobrojan@gmail.com>
2025-03-30 21:39:46 +0300

    Small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { lazy, Suspense, useState } from 'react'
 import type { Task } from '@/types/task'
 import TaskModal from '@/components/TaskModal'
 import tasksJson from '@/data/tasks.json'
-import './leaflet.css'
 import './app.css'
+import { cn } from '@/utils/cn.ts'
 
 const Map = lazy(() => import('@/components/Map'))
 
@@ -28,19 +28,34 @@ export default function App() {
     <main className="">
       <Suspense fallback={<h2>Loading map ...</h2>}>
         <Map
+          width="70%"
+          height="100vh"
           selectedCountry={selectedCountry}
           disabled={!!selectedCountry}
           tasks={tasks}
           onCountrySelect={handleCountrySelect}
         />
       </Suspense>
-      {selectedCountry && (
-        <TaskModal
-          onClose={() => setSelectedCountry(null)}
-          onComplete={handleCompleteTask}
-          task={tasks.find((t) => t.country === selectedCountry)!}
-        />
-      )}
+      <ul className="w-[30%] inline-block h-screen max-h-screen overflow-y-auto">
+        {tasks.map((t) => (
+          <li
+            key={t.country}
+            className={cn(
+              'cursor-pointer px-3 py-0.5 hover:bg-white/10 duration-150',
+              selectedCountry === t.country && 'bg-white/10',
+            )}
+            onClick={() => setSelectedCountry(t.country)}
+          >
+            {t.country}
+          </li>
+        ))}
+      </ul>
+      <TaskModal
+        isOpen={!!selectedCountry}
+        onClose={() => setSelectedCountry(null)}
+        onComplete={handleCompleteTask}
+        task={tasks.find((t) => t.country === selectedCountry)!}
+      />
     </main>
   )
 }
