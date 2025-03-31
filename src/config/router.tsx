f6d3f1df4dd8ae29c463526e4ab42bfd197813d5
warek <alexandrudobrojan@gmail.com>
2025-03-31 21:43:25 +0300

    Add hash router

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -1,30 +1,35 @@
-import { createBrowserRouter } from 'react-router'
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouteObject,
+} from 'react-router'
 
 import HomePage from '@/pages/home.page.tsx'
 import MapPage from '@/pages/map.page.tsx'
 import TasksPage from '@/pages/tasks.page.tsx'
 import App from '@/App'
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <App />,
-      children: [
-        {
-          index: true,
-          element: <HomePage />,
-        },
-        {
-          path: 'map',
-          element: <MapPage />,
-        },
-        {
-          path: 'tasks',
-          element: <TasksPage />,
-        },
-      ],
-    },
-  ],
-  { basename: import.meta.env.BASE_URL },
-)
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'map',
+        element: <MapPage />,
+      },
+      {
+        path: 'tasks',
+        element: <TasksPage />,
+      },
+    ],
+  },
+]
+
+export const router = import.meta.env.PROD
+  ? createHashRouter(routes)
+  : createBrowserRouter(routes, { basename: import.meta.env.BASE_URL })
